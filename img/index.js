6959e807c584a7dcb55ce9b02e3be09881ba8f5a
moinhaider520 <moin.haider.520@gmail.com>
2023-01-31 13:34:34 -0800

    Second commit

---
+++ img/index.js
@@ -0,0 +1,14 @@
+
+import nft_1 from "./nft_1.png";
+
+import nft_image_1 from "./nft-image-1.png";
+import nft_image_2 from "./nft-image-2.png";
+import nft_image_3 from "./nft-image-3.png";
+
+
+export default {
+  nft_image_1,
+  nft_image_2,
+  nft_image_3,
+  nft_1,
+};
